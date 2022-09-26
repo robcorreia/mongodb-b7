e1cfe1c58e23e0abd027361af1a0b1e450596d21
Robson <robccorreia@gmail.com>
2022-09-26 19:09:08 -0300

    feat: update user

--- project-01/src/controllers/homeController.ts
+++ project-01/src/controllers/homeController.ts
@@ -1,9 +1,22 @@
 import { Request, Response } from "express";
 
-import { Product } from "../models/Product";
 import User from "../models/User";
 
 export const home = async (req: Request, res: Response) => {
+  let user = await User.findOneAndUpdate(
+    { "name.firstName": "Robson" },
+    { "name.lastName": "Correia Fantin" }
+  );
+
+  // let andre = await User.findOne({ email: "andre@gmail.com" });
+  // if (andre) {
+  //   andre.name.lastName = "Tiozão Silva";
+  //   await andre.save();
+  // }
+  // console.log(andre);
+
+  // await User.updateMany({ age: { $lte: 18 } }, { age: 18 });
+
   let users = await User.find({}).sort({ "name.firstName": 1 });
 
   res.render("pages/home", {
